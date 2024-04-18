745133e9473194f4cf092866410222d8fe272fa9
김영욱 <dmnion@naver.com>
2024-04-18 17:39:51 +0900

    Update backend/src/main/java/com/ssafy/apm/gameuser/controller/GameUserController.java

--- backend/src/main/java/com/ssafy/apm/gameuser/controller/GameUserController.java
+++ backend/src/main/java/com/ssafy/apm/gameuser/controller/GameUserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/gameuser")
+@RequestMapping("/api/game-user")
 @RequiredArgsConstructor
 @Slf4j
 public class GameUserController {
