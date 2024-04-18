4869d8bce452d36a7bf9db2e9b5f0565a7869388
김영욱 <dmnion@naver.com>
2024-04-18 17:07:02 +0900

    :sparkles: feat: Game 도메인 스켈레톤 코드 작성

---
+++ backend/src/main/java/com/ssafy/apm/game/service/GameServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssafy.apm.game.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class GameServiceImpl implements GameService {
+}
