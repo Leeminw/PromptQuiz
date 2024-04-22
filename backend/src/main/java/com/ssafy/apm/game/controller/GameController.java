98d60edc2c18f8e7b8172c264035d8f21a105ced
김영욱 <dmnion@naver.com>
2024-04-22 17:52:41 +0900

    feat: GameRoom 리스트 조회 코드 작성 완료, 테스트 필요

--- backend/src/main/java/com/ssafy/apm/game/controller/GameController.java
+++ backend/src/main/java/com/ssafy/apm/game/controller/GameController.java
@@ -2,12 +2,15 @@ package com.ssafy.apm.game.controller;
 
 import com.ssafy.apm.common.domain.ResponseData;
 import com.ssafy.apm.game.dto.request.GameCreateRequestDto;
+import com.ssafy.apm.game.dto.response.GameGetResponseDto;
 import com.ssafy.apm.game.service.GameServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/game")
@@ -25,8 +28,9 @@ public class GameController {
     }
 
     @GetMapping("/getGameList")
-    public ResponseEntity<ResponseData<?>> getGameList() {
-        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success());
+    public ResponseEntity<ResponseData<?>> getGameList(@RequestParam Long channelId) {
+        List<GameGetResponseDto> dtoList = gameService.getGameList(channelId);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseData.success(dtoList));
     }
 
     @DeleteMapping()
--- backend/src/main/java/com/ssafy/apm/game/service/GameServiceImpl.java
+++ backend/src/main/java/com/ssafy/apm/game/service/GameServiceImpl.java
@@ -2,12 +2,15 @@ package com.ssafy.apm.game.service;
 
 import com.ssafy.apm.game.domain.GameEntity;
 import com.ssafy.apm.game.dto.request.GameCreateRequestDto;
+import com.ssafy.apm.game.dto.response.GameGetResponseDto;
 import com.ssafy.apm.game.repository.GameRepository;
 import com.ssafy.apm.gameuser.domain.GameUserEntity;
 import com.ssafy.apm.gameuser.repository.GameUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +34,15 @@ public class GameServiceImpl implements GameService {
                 .ranking(0)
                 .build();
         gameUserRepository.save(gameUserEntity);
+    }
+
+    @Override
+    public List<GameGetResponseDto> getGameList(Long channelId) {
+        List<GameEntity> entityList = gameRepository.findAllByChannelId(channelId);
+
+        return entityList.stream()
+                .map(GameGetResponseDto::new)
+                .toList();
+
     }
 }
