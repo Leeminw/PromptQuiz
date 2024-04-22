faef741b1b04750b936c59a42ce7c3c6da195ec7
서다찬 <chn7894@naver.com>
2024-04-22 16:54:27 +0900

    test: GET /socket/room/connect 소켓 연결 테스트

--- backend/src/test/java/com/ssafy/apm/common/SocketControllerTests.java
+++ backend/src/test/java/com/ssafy/apm/common/SocketControllerTests.java
@@ -1,8 +1,6 @@
 package com.ssafy.apm.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.apm.common.controller.SocketController;
-import com.ssafy.apm.common.service.SocketServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,25 +8,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@Slf4j
 @WebMvcTest(SocketController.class)
 @ExtendWith(MockitoExtension.class)
-@Slf4j
+@ComponentScan({"com.ssafy.apm.common"})
 public class SocketControllerTests {
 
     @Autowired
     private MockMvc mock;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private SocketServiceImpl socketService;
 
     @Test
     void build() {
@@ -38,10 +31,8 @@ public class SocketControllerTests {
     @Test
     @DisplayName("소켓 연결 테스트")
     void connectionTest() throws Exception {
-        String roomId = "test";
-
         // socket connect get test
-        mock.perform(MockMvcRequestBuilders.get("/socket/room/connect/" + roomId)
+        mock.perform(MockMvcRequestBuilders.get("/socket/room/connect")
                         .content("connected")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
