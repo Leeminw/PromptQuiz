4e52e4977d78c9a595b94ca777bf87181bbf1df9
서다찬 <chn7894@naver.com>
2024-04-23 15:34:49 +0900

    faet: GameResponseDto 게임방에서 사용되는 소켓 response dto 추가

---
+++ backend/src/main/java/com/ssafy/apm/common/dto/response/GameResponseDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.apm.common.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class GameResponseDto {
+    // 소켓 메세지 태그를 나타냅니다 (game, lobby, chat)
+    String tag;
+
+    // data에는 내용에 해당하는 response dto가 들어갑니다.
+    Object data;
+}
