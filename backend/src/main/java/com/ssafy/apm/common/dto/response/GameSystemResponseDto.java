bcbbd25ffddc6a02b5beab809b95b616b0289c9e
서다찬 <chn7894@naver.com>
2024-04-23 15:35:36 +0900

    feat: GameSystemResponseDto 게임 진행 관련 response dto 추가

---
+++ backend/src/main/java/com/ssafy/apm/common/dto/response/GameSystemResponseDto.java
@@ -0,0 +1,37 @@
+package com.ssafy.apm.common.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class GameSystemResponseDto {
+    // 게임의 타입 분류 -> ready, start, end, result
+    String type;
+    
+    // content는 각각 상태에 들어갈 내용
+    // GameChat or GameSystemContentDto
+    Object content;
+
+    // 라운드 준비 상태 response
+    public static GameSystemResponseDto ready(Object content){
+        return new GameSystemResponseDto("ready", content);
+    }
+
+    // 라운드 시작 상태 response
+    public static GameSystemResponseDto start(Object content){
+        return new GameSystemResponseDto("start", content);
+    }
+
+    // 라운드 종료 상태 response
+    public static GameSystemResponseDto end(Object content){
+        return new GameSystemResponseDto("end", content);
+    }
+
+    // 전체 게임 종료 response
+    public static GameSystemResponseDto result(Object content){
+        return new GameSystemResponseDto("result", content);
+    }
+}
