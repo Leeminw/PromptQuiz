89b458ea6c976a8837f0df9f482d7fc29ea792b9
서다찬 <chn7894@naver.com>
2024-04-24 15:35:02 +0900

    feat: QuizService 유사도 측정 메서드 구현

--- backend/src/main/java/com/ssafy/apm/quiz/service/QuizServiceImpl.java
+++ backend/src/main/java/com/ssafy/apm/quiz/service/QuizServiceImpl.java
@@ -7,7 +7,12 @@ import com.ssafy.apm.quiz.dto.response.QuizDetailResponseDto;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Service
@@ -19,5 +24,21 @@ public class QuizServiceImpl implements QuizService{
     public QuizDetailResponseDto getQuizInfo(Long quizId) {
         Quiz entity = repository.findById(quizId).orElseThrow(() -> new QuizNotFoundException(quizId));
         return new QuizDetailResponseDto(entity);
+    }
+
+    // 라벤슈타인 문자열 유사도 측정 코드
+    public double calcSimilarity(String input, String answer) {
+        // 공백 제거하기
+        input = input.replace(" ","");
+        answer = answer.replace(" ", "");
+        
+        LevenshteinDistance ld = new LevenshteinDistance();
+        int maxLen = Math.max(input.length(), answer.length());
+
+        double result = 0;
+        double temp = ld.apply(input, answer);
+        result = (maxLen - temp) / maxLen;
+
+        return result;
     }
 }
---
+++ backend/src/main/java/com/ssafy/apm/quiz/util/QuizDetail.java
@@ -0,0 +1,8 @@
+package com.ssafy.apm.quiz.util;
+
+public class QuizDetail {
+    // 객관식, 주관식, 순서 타입
+    public String type;
+
+    public Object data;
+}
