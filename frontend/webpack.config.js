c0d55470381ea7b9b11ae6ed711817f381a5dbaa
조시훈 <kepler452k@naver.com>
2024-04-24 15:07:27 +0900

    feat: module export name 추가

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  name: 'your_project_name',
+  name: 'AI-Prompt-Matcher',
   entry: './src/index.tsx',
   module: {
     rules: [
