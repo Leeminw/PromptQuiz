417e4a7d6e4ebded0497cd8bf928c9097dd63223
서다찬 <chn7894@naver.com>
2024-04-24 13:23:18 +0900

    feat: QuizEntity 추가

--- backend/src/main/java/com/ssafy/apm/quiz/domain/QuizEntity.java
+++ backend/src/main/java/com/ssafy/apm/quiz/domain/QuizEntity.java
@@ -1,4 +1,27 @@
 package com.ssafy.apm.quiz.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "quiz")
 public class QuizEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long promptGroup;
+    private String image;
+    private String prompt;
+    private Integer style;
 }
--- backend/src/main/java/com/ssafy/apm/quiz/repository/QuizRepository.java
+++ backend/src/main/java/com/ssafy/apm/quiz/repository/QuizRepository.java
@@ -1,4 +1,7 @@
 package com.ssafy.apm.quiz.repository;
 
-public interface QuizRepository {
+import com.ssafy.apm.quiz.domain.QuizEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuizRepository  extends CrudRepository<QuizEntity, Long> {
 }
