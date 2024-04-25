a8b9e16e19d67803a7d90c625333ac6ebb23723f
yeogeru <yjw0835@naver.com>
2024-04-25 15:25:22 +0900

    fix: Login page add

--- frontend/src/utils/router.tsx
+++ frontend/src/utils/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from '../App';
+import LoginPage from '../pages/Login';
 import Join from '../pages/Join';
 import ChannelSelectPage from '../pages/ChannelSelect';
 
@@ -8,6 +9,11 @@ const RouterInfo = [
     path: '/',
     element: <App />,
     children: [
+      {
+        index: true,
+        element: <LoginPage />,
+        label: 'login',
+      },
       {
         path: '/join',
         element: <Join />,
