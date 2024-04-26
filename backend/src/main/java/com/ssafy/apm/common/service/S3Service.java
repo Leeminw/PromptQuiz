321362f9c2a299d92cdded9e7e7feb8afdf10503
조시훈 <kepler452k@naver.com>
2024-04-26 17:33:46 +0900

    feat: AWS S3 퍄일관리 Service 추가

--- backend/src/main/java/com/ssafy/apm/common/service/S3Service.java
+++ backend/src/main/java/com/ssafy/apm/common/service/S3Service.java
@@ -1,9 +1,13 @@
 package com.ssafy.apm.common.service;
 
+import com.ssafy.apm.common.dto.response.S3FileResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface S3Service {
-    public String uploadFile(MultipartFile multipartFile) throws IOException;
+    S3FileResponseDto uploadFile(MultipartFile file) throws IOException;
+    S3FileResponseDto uploadImage(MultipartFile file) throws IOException;
+    S3FileResponseDto uploadVideo(MultipartFile file) throws IOException;
+
 }
--- backend/src/main/java/com/ssafy/apm/common/service/S3ServiceImpl.java
+++ backend/src/main/java/com/ssafy/apm/common/service/S3ServiceImpl.java
@@ -2,7 +2,9 @@ package com.ssafy.apm.common.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.ssafy.apm.common.dto.request.S3FileRequestDto;
+import com.ssafy.apm.common.dto.response.S3FileResponseDto;
+import com.ssafy.apm.common.repository.S3FileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,18 +12,65 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3ServiceImpl implements S3Service {
-    private static final String S3_BASE_URL = "https://%s.s3.amazonaws.com/";
-    private static final String S3KEY_PREFIX = "images/";
+
+    private final S3FileRepository s3FileRepository;
 
     private final AmazonS3 amazonS3;
 
     @Value("${cloud.aws.bucket.name}")
     private String bucketName;
+
+    @Override
+    public S3FileResponseDto uploadFile(MultipartFile file) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        String filename = "files/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+        return uploadFileToS3(file, metadata, filename);
+    }
+
+    @Override
+    public S3FileResponseDto uploadImage(MultipartFile file) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        String filename = "images/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+        return uploadFileToS3(file, metadata, filename);
+    }
+
+    @Override
+    public S3FileResponseDto uploadVideo(MultipartFile file) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        String filename = "videos/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+        return uploadFileToS3(file, metadata, filename);
+    }
+
+    private S3FileResponseDto uploadFileToS3(MultipartFile file, ObjectMetadata metadata, String filename) throws IOException {
+        amazonS3.putObject(bucketName, filename, file.getInputStream(), metadata);
+
+        String url = String.format("https://%s.s3.amazonaws.com/%s", bucketName, filename);
+        S3FileRequestDto requestDto = S3FileRequestDto.builder()
+                .originalFilename(file.getOriginalFilename())
+                .filetype(file.getContentType())
+                .filesize(file.getSize())
+                .filename(filename)
+                .url(url)
+                .build();
+
+        return new S3FileResponseDto(s3FileRepository.save(requestDto.toEntity()));
+    }
+
 
 //    public String uploadFileToS3(MultipartFile multipartFile) {
 //        String s3Key = S3KEY_PREFIX + multipartFile.getOriginalFilename();
@@ -32,13 +81,13 @@ public class S3ServiceImpl implements S3Service {
 //        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, amazonS3.getRegionName(), s3Key);
 //    }
 
-    @Override
-    public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
-        return fileName;
-    }
+//    @Override
+//    public String uploadFile(MultipartFile file) throws IOException {
+//        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+//        ObjectMetadata metadata = new ObjectMetadata();
+//        metadata.setContentLength(file.getSize());
+//        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata));
+//        return fileName;
+//    }
 
 }
