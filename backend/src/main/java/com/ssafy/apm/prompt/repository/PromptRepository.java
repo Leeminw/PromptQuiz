52da051b9f626b6cba6ad165bd0609bc35e2ef36
조시훈 <kepler452k@naver.com>
2024-04-28 13:57:20 +0900

    feat: Prompt Repository 구현

---
+++ backend/src/main/java/com/ssafy/apm/prompt/repository/PromptRepository.java
@@ -0,0 +1,25 @@
+package com.ssafy.apm.prompt.repository;
+
+import com.ssafy.apm.prompt.domain.Prompt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PromptRepository extends JpaRepository<Prompt, Long> {
+    Optional<List<Prompt>> findAllByStyle(String style);
+    Optional<List<Prompt>> findAllByGroup(Integer group);
+
+    @Query(value = "SELECT * FROM prompt ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<Prompt> extractRandomPrompt();
+
+    @Query(value = "SELECT * FROM prompt ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    Optional<List<Prompt>> extractRandomPrompts(@Param("limit") Integer limit);
+
+    @Query(value = "SELECT * FROM prompt WHERE group = :group ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    Optional<List<Prompt>> extractRandomPromptsByGroup(@Param("group") Integer group, @Param("limit") Integer limit);
+}
