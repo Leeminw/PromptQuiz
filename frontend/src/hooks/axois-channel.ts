be18a488460cc9f6ebe9d2383d4604d72db4ea19
서해광 <you@example.com>
2024-05-02 10:20:09 +0900

    feat : 채널 목록 조회 API 연결 완료

---
+++ frontend/src/hooks/axois-channel.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import instance from "./axios-instance";
+import useUserStore from "../stores/userStore";
+const BASE_URL = process.env.REACT_APP_SERVER
+
+/**
+ * /api/v1/channel/getChannelList 오타인듯
+ * 
+ */
+
+ const ChannelApi= {
+    // 채널 목록 조회
+    getChannelList : async () => {
+        try{
+            const response = await axios.get(
+                BASE_URL + `/channel/channelList`, 
+            )
+            return response.data
+        }
+        catch (error) {
+            console.error(error)
+            return Promise.reject(error)
+        }
+    },
+}
+export{ChannelApi}
