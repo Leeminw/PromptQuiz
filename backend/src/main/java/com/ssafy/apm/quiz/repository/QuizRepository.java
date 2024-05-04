d7e3830d461bc39b053a2db032468d1fea688219
조시훈 <kepler452k@naver.com>
2024-05-04 16:10:36 +0900

    refactor: 공백 및 함수명 리팩토링

--- backend/src/main/java/com/ssafy/apm/quiz/repository/QuizRepository.java
+++ backend/src/main/java/com/ssafy/apm/quiz/repository/QuizRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.apm.quiz.repository;
 
-import com.ssafy.apm.prompt.domain.Prompt;
 import com.ssafy.apm.quiz.domain.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+
     Optional<List<Quiz>> findAllByStyle(String style);
     Optional<List<Quiz>> findAllByGroupCode(String groupCode);
 
@@ -19,12 +19,12 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
     Optional<Quiz> extractRandomQuiz();
 
     @Query(value = "SELECT * FROM quiz ORDER BY RAND() LIMIT :limit", nativeQuery = true)
-    Optional<List<Quiz>> extractRandomQuizs(@Param("limit") Integer limit);
+    Optional<List<Quiz>> extractRandomQuizzes(@Param("limit") Integer limit);
 
     @Query(value = "SELECT * FROM quiz WHERE style = :style ORDER BY RAND() LIMIT :limit", nativeQuery = true)
-    Optional<List<Quiz>> extractRandomQuizsByStyle(@Param("style") String style, @Param("limit") Integer limit);
+    Optional<List<Quiz>> extractRandomQuizzesByStyle(@Param("style") String style, @Param("limit") Integer limit);
 
     @Query(value = "SELECT * FROM quiz WHERE group_code = :groupCode ORDER BY RAND() LIMIT :limit", nativeQuery = true)
-    Optional<List<Quiz>> extractRandomQuizsByGroupCode(@Param("groupCode") String groupCode, @Param("limit") Integer limit);
+    Optional<List<Quiz>> extractRandomQuizzesByGroupCode(@Param("groupCode") String groupCode, @Param("limit") Integer limit);
 
 }
