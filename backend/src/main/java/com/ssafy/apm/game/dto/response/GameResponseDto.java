d1d2c0dfc01183e28016c8ef9be61cb4b503597b
조시훈 <kepler452k@naver.com>
2024-05-07 17:31:34 +0900

    fix: Game Response Dto 수정

---
+++ backend/src/main/java/com/ssafy/apm/game/dto/response/GameResponseDto.java
@@ -0,0 +1,43 @@
+package com.ssafy.apm.game.dto.response;
+
+import com.ssafy.apm.game.domain.Game;
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameResponseDto {
+    private String code;
+    private String channelCode;
+    private String password;
+    private String title;
+    private Integer mode;
+    private Integer style;
+    private Boolean isTeam;
+    private Boolean isPrivate;
+    private Boolean isStarted;
+    private Integer timeLimit;
+    private Integer curRounds;
+    private Integer maxRounds;
+    private Integer curPlayers;
+    private Integer maxPlayers;
+
+    public GameResponseDto(Game entity) {
+        this.code = entity.getCode();
+        this.channelCode = entity.getChannelCode();
+        this.password = entity.getPassword();
+        this.title = entity.getTitle();
+        this.mode = entity.getMode();
+        this.style = entity.getStyle();
+        this.isTeam = entity.getIsTeam();
+        this.isPrivate = entity.getIsPrivate();
+        this.isStarted = entity.getIsStarted();
+        this.timeLimit = entity.getTimeLimit();
+        this.curRounds = entity.getCurRounds();
+        this.maxRounds = entity.getMaxRounds();
+        this.curPlayers = entity.getCurPlayers();
+        this.maxPlayers = entity.getMaxPlayers();
+    }
+}
