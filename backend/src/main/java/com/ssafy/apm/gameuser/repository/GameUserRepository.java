88e9df19a4adde570308e8d94e1fe3911f12d0e0
조시훈 <kepler452k@naver.com>
2024-05-08 02:31:27 +0900

    refactor: GameUser Repository, Service 리팩토링

--- backend/src/main/java/com/ssafy/apm/gameuser/repository/GameUserRepository.java
+++ backend/src/main/java/com/ssafy/apm/gameuser/repository/GameUserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface GameUserRepository extends CrudRepository<GameUser, Long> {
+public interface GameUserRepository extends CrudRepository<GameUser, String> {
 
     Optional<GameUser> findByUserId(Long userId);
 
