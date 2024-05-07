e5caa57f8a304d78d660a71056ab65f9a4fd0c76
조시훈 <kepler452k@naver.com>
2024-05-07 17:36:05 +0900

    fix: GameUserRepository 수정

--- backend/src/main/java/com/ssafy/apm/gameuser/repository/GameUserRepository.java
+++ backend/src/main/java/com/ssafy/apm/gameuser/repository/GameUserRepository.java
@@ -1,23 +1,19 @@
 package com.ssafy.apm.gameuser.repository;
 
-import com.ssafy.apm.gameuser.domain.GameUserEntity;
-import com.ssafy.apm.gameuser.dto.response.GameUserDetailResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import com.ssafy.apm.gameuser.domain.GameUser;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface GameUserRepository extends CrudRepository<GameUserEntity, Long> {
+public interface GameUserRepository extends CrudRepository<GameUser, Long> {
 
-    Optional<List<GameUserEntity>> findAllByGameId(Long gameId);
+    Optional<GameUser> findByUserId(Long userId);
 
-    Optional<GameUserEntity> findByGameIdAndUserId(Long gameId, Long UserId);
+    Optional<GameUser> findByGameCodeAndUserId(String gameCode, Long userId);
 
-    Optional<GameUserEntity> findByUserId(Long userId);
+    Optional<List<GameUser>> findAllByGameCode(String gameCode);
 
 }
