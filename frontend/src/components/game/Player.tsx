5a88107efb86cffe14b7fc5b446695a830e0c7a4
leeminwoo <asdfn0@naver.com>
2024-05-07 10:58:13 +0900

    feat: chatting 말풍선 연동

--- frontend/src/components/game/Player.tsx
+++ frontend/src/components/game/Player.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } f
 interface GamePlayerProps {
   // idx: number;
   userInfo: GameUser | null;
+  message: string | null;
 }
 // { idx }: GamePlayerProps
 // const GamePlayer = forwardRef((props, ref) => {
@@ -10,19 +11,24 @@ interface GamePlayerProps {
 //     console.log('HI');
 //   };
 
-const GamePlayer = ({ userInfo }: GamePlayerProps) => {
+const GamePlayer = ({ userInfo, message }: GamePlayerProps) => {
+  // const timeout;
+
   useEffect(() => {
-    console.log(userInfo);
-  }, []);
+    setTimeout(() => {
+      message = '';
+      // 메시지 끝 >> 부모한테 보내고
+      // 받으면 다시갱신 ?
+    }, 3000);
+  }, [message]);
   return (
     <div className="border-custom-green bg-customGreen w-full h-full relative flex items-center">
       <div className="absolute w-full -translate-y-8">
         <div
-          // ref={chatBubbleBox}
           className="min-w-20 min-h-6 w-fit h-fit bg-white border border-gray-200 rounded-lg text-sm px-2 py-0.5 line-clamp-2 text-black
         "
         >
-          가
+          {message}
         </div>
         <svg className="absolute z-10 w-3 h-3 translate-x-8 -translate-y-[0.05rem]">
           <path d="M 0 0 V 10 L 7 0" stroke="#dde5e3" strokeWidth={1} fill="white"></path>
