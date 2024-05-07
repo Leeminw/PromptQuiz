d8c62d167b89f2cba97f03a31cea83ba6ebd6b09
조시훈 <kepler452k@naver.com>
2024-05-07 17:32:56 +0900

    fix: GameUser Entity 수정

--- backend/src/main/java/com/ssafy/apm/gameuser/domain/GameUser.java
+++ backend/src/main/java/com/ssafy/apm/gameuser/domain/GameUser.java
@@ -6,37 +6,29 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
-@ToString
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@RedisHash(value = "game-user", timeToLive = 3600)
-public class GameUserEntity {
+@RedisHash(timeToLive = 3600)
+public class GameUser {
     @Id
-    private Long id;
+    private String code;
     @Indexed
-    private Long gameId;
+    private String gameCode;
     @Indexed
     private Long userId;
-    private Boolean isHost;
-    private Boolean isReady;
-    private Integer score;
     private String team;
-    private Integer ranking;
-
-    //    정답, 오답시 스코어 더해주는 API
-    public void updateScore(Integer score) {
-        this.score += score;
-    }
-
-    //    레디 상태 변경
-    public void updateIsReady(Boolean isReady) {
-        this.isReady = isReady;
-    }
+    private Integer score;
+    private Boolean isHost;
 
     //    팀 상태 변경
     public void updateTeam(String team) {
         this.team = team;
+    }
+
+    //    정답, 오답시 스코어 더해주는 API
+    public void updateScore(Integer score) {
+        this.score += score;
     }
 
     public void updateIsHost(Boolean isHost) {
