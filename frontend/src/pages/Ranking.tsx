ac76f4f80beeb13763197163a9964d60b69122fc
서해광 <you@example.com>
2024-05-08 10:14:10 +0900

    feat : 랭킹 axios API 연결 완료

--- frontend/src/pages/Ranking.tsx
+++ frontend/src/pages/Ranking.tsx
@@ -1,7 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import RankingList from '../components/ranking/RankingList';
+import { RankApi } from '../hooks/axios-rank';
 const Ranking = () => {
+  const [rankingArray, setRankingArray] = useState<Ranking[]>([]);
+  const [teamRankingArray, setTeamRankingArray] = useState<Ranking[]>([]);
+  const [soloRankingArray, setSoloRankingArray] = useState<Ranking[]>([]);
+  const [totalRankingArray, setTotalRankingArray] = useState<Ranking[]>([]);
+  useEffect(() => {
+    // 랭킹 리스트 가져오기
+    const response = RankApi.getRankingList()
+      .then((response) => {
+        setTeamRankingArray(response.data.teamRanking);
+        setSoloRankingArray(response.data.soloRanking);
+        setTotalRankingArray(response.data.totalRanking);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   const obj = {
     status: 'success',
     message: 'The request has been processed successfully.',
@@ -306,9 +323,10 @@ const Ranking = () => {
     <div className=" w-3/4 h-3/4 bg-white opacity-80">
       <div>이건 랭킹임 ㅋㅋㅋ</div>
       <div className="flex flex-row items-center">
-        <RankingList rankingTitle={'통합 랭킹'} users={obj.data.totalRanking} />
-        <RankingList rankingTitle={'개인전 랭킹'} users={obj.data.soloRanking} />
-        <RankingList rankingTitle={'팀 랭킹'} users={obj.data.teamRanking} />
+        <RankingList rankingTitle={'통합 랭킹'} users={totalRankingArray} />
+        <RankingList rankingTitle={'개인전 랭킹'} users={soloRankingArray} />
+        <RankingList rankingTitle={'팀 랭킹'} users={teamRankingArray} />
+        {/* <RankingList rankingTitle={'팀 랭킹'} users={obj.data.teamRanking} /> */}
       </div>
       <Link to="/channel">
         <button
