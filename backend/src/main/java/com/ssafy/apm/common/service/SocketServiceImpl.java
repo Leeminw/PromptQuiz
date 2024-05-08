1cf1dcf56c982d79027142c5d00142c52f64e147
서다찬 <chn7894@naver.com>
2024-05-08 17:48:44 +0900

    fix: 소켓 연결 오류 해결

--- backend/src/main/java/com/ssafy/apm/common/service/SocketServiceImpl.java
+++ backend/src/main/java/com/ssafy/apm/common/service/SocketServiceImpl.java
@@ -5,9 +5,11 @@ import com.ssafy.apm.common.repository.SocketRepository;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -18,35 +20,50 @@ public class SocketServiceImpl implements SocketService {
     @Override
     @Transactional
     public void addSession(String sessionId) {
-        socketRepository.save(new Session(sessionId, 1L, "0", 2));
+        try {
+            socketRepository.save(new Session(sessionId, 1L, "0", 2));
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
     }
 
     @Override
     @Transactional
     public void kickOutUser(String sessionId) {
+        try {
+            Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
 
-        Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
+            // todo: 현재 있는 채널 혹은 게임에서 강퇴시키기
+            if (session.getType() == 1) {
 
-        // todo: 현재 있는 채널 혹은 게임에서 강퇴시키기
-        if (session.getType() == 1) {
+            } else {
 
-        } else {
-
+            }
+        } catch (Exception e) {
+            log.debug(e.getMessage());
         }
     }
 
     @Override
     @Transactional
     public void editSession(String sessionId, String uuid, Integer type) {
-        Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
-        session.updateState(uuid, type);
+        try {
+            Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
+            session.updateState(uuid, type);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
     }
 
     @Override
     @Transactional
     public void deleteSession(String sessionId) {
-        Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
-        socketRepository.delete(session);
+        try {
+            Session session = socketRepository.findBySessionId(sessionId).orElseThrow();
+            socketRepository.delete(session);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
     }
 
 }
