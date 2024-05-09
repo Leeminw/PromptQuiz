1f48124503de6235c1567e795c104f64dcaa6839
서다찬 <chn7894@naver.com>
2024-05-09 17:01:13 +0900

    refactor: Ready dto 수정

--- backend/src/main/java/com/ssafy/apm/socket/dto/request/GameReadyDto.java
+++ backend/src/main/java/com/ssafy/apm/socket/dto/request/GameReadyDto.java
@@ -1,10 +1,14 @@
 package com.ssafy.apm.socket.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class GameReadyDto {
 
     private String gameCode;
