84681f576bb8bbf0a028e66f972479df57cfd95a
서다찬 <chn7894@naver.com>
2024-05-14 17:58:19 +0900

    refactor: 채널 메세지 수정

--- backend/src/main/java/com/ssafy/apm/channel/controller/ChannelSocketController.java
+++ backend/src/main/java/com/ssafy/apm/channel/controller/ChannelSocketController.java
@@ -30,14 +30,12 @@ public class ChannelSocketController {
 
     @MessageMapping("/channel/enter")
     public void sendChannelEnterMessage(@Payload ChannelChatRequestDto chatMessage) {
-        ChannelChatResponseDto chat = chatService.insertChannelChat(chatMessage);
-        template.convertAndSend("/ws/sub/channel?uuid=" + chat.getUuid(), chat);
+        template.convertAndSend("/ws/sub/channel?uuid=" + chatMessage.getUuid(), "enter");
     }
 
     @MessageMapping("/channel/leave")
     public void sendChannelLeaveMessage(@Payload ChannelChatRequestDto chatMessage) {
-        ChannelChatResponseDto chat = chatService.insertChannelChat(chatMessage);
-        template.convertAndSend("/ws/sub/channel?uuid=" + chat.getUuid(), chat);
+        template.convertAndSend("/ws/sub/channel?uuid=" + chatMessage.getUuid(), "leave");
     }
 
 }
