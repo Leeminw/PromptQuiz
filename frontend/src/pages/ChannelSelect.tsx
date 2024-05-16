e9e2a28b43daf14d6eb225096de187d561773c2b
yeogeru <yjw0835@naver.com>
2024-05-16 12:25:08 +0900

    fix: game ui fix

--- frontend/src/pages/ChannelSelect.tsx
+++ frontend/src/pages/ChannelSelect.tsx
@@ -3,6 +3,7 @@ import ChannelBox from '../components/channel/ChannelBox';
 import { ChannelApi } from '../hooks/axois-channel';
 import { useNavigate } from 'react-router-dom';
 import { IoLogOut } from 'react-icons/io5';
+import CustomButton from '../components/ui/CustomButton';
 
 const ChannelSelectPage = () => {
   const [channelArray, setChannelList] = useState<Channel[]>([]);
@@ -19,69 +20,22 @@ const ChannelSelectPage = () => {
       });
   }, []);
 
-  // 버튼 클릭 시 이벤트 처리
-  const handleClick = (id: number) => {
-    setActivateBtn((prev) => ({ ...prev, [id]: true }));
-    console.log(activateBtn);
-    setTimeout(() => {
-      setActivateBtn((prev) => ({ ...prev, [id]: false }));
-    }, 400);
+  // 버튼 제어
+  const [btnCurrentActivate, setBtnCurrentActivate] = useState<boolean>(false);
+  const activateBtnFunc = async () => {
+    setBtnCurrentActivate(true);
+    await setTimeout(() => {
+      setBtnCurrentActivate(false);
+    }, 800);
   };
 
-  const headerStyle = {
-    width: '100%',
-    height: '50px',
-    color: '#359DB0',
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    opacity: '0.7',
-    fontSize: '20px',
-  };
-  const channelList = {
-    width: '100%',
-    height: '400px',
-    color: '#359DB0',
-    display: 'flex',
-    flexFlow: 'row wrap',
-    // justifyContent: 'center',
-    backgroundColor: 'white',
-    opacity: '0.7',
-    marginTop: '10px',
-  };
   return (
-    <div
-      className="container"
-      style={
-        {
-          //   display: 'flex',
-          //   justifyContent: 'center',
-        }
-      }
-    >
-      <div style={headerStyle}>
-        <p className='w-full text-center'>채널목록</p>
-        <button
-          className={`btn-red text-white hover:brightness-125 hover:scale-105 transition 
-            text-sm w-28 min-w-[3rem] ${activateBtn[1] ? 'animate-clickbtn scale-105' : ''}`}
-          onClick={() => {
-            handleClick(1);
-            setTimeout(() => {
-              navigate('/');
-            }, 500);
-          }}
-        >
-          <label className="flex gap-1 items-center px-2 cursor-pointer overflow-hidden max-xl:justify-center">
-            <IoLogOut className="min-w-6 min-h-6 mb-0.5" />
-            <p className="text-center w-full text-nowrap text-sm overflow-hidden text-ellipsis xl:flex max-xl:hidden">
-              로그아웃
-            </p>
-          </label>
-        </button>
+    <div className="w-[55rem] min-w-[45rem]">
+      <div className="w-full flex justify-center items-center bg-white/90 backdrop-blur-sm h-14 rounded-xl">
+        <p className="text-mint font-extrabold text-lg">채널목록</p>
       </div>
 
-      <div style={channelList}>
-        {/* <ChannelBox id={1} code="code1" name="채널1" curPlayers={10} maxPlayers={100} /> */}
+      <div className="grid grid-cols-3 grid-rows-3 gap-x-3 gap-y-3 py-4 rounded-xl mt-2 mb-4">
         {channelArray.map((item, idx) => (
           <ChannelBox
             key={idx}
@@ -90,8 +44,29 @@ const ChannelSelectPage = () => {
             name={item.name}
             curPlayers={item.curPlayers}
             maxPlayers={item.maxPlayers}
+            btnActivate={btnCurrentActivate}
+            onClick={() => {
+              activateBtnFunc();
+            }}
           />
         ))}
+      </div>
+      <div className="flex justify-end">
+        <CustomButton
+          className="btn-red text-white text-sm w-28 min-w-[3rem] flex gap-1 items-center px-2 overflow-hidden max-xl:justify-center"
+          onClick={() => {
+            activateBtnFunc();
+            setTimeout(() => {
+              navigate('/');
+            }, 500);
+          }}
+          btnCurrentActivate={btnCurrentActivate}
+        >
+          <IoLogOut className="min-w-6 min-h-6 mb-0.5" />
+          <p className="text-center w-full text-nowrap text-sm overflow-hidden text-ellipsis">
+            로그아웃
+          </p>
+        </CustomButton>
       </div>
     </div>
   );
