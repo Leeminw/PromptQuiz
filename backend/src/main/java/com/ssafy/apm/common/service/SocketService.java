3ac41b527de90840b4bfc30dc6494fb8b3309e6d
서다찬 <chn7894@naver.com>
2024-05-16 10:41:38 +0900

    feat: 소켓 연결 끊겼을 시 메세지 전송 로직 추가

--- backend/src/main/java/com/ssafy/apm/common/service/SocketService.java
+++ backend/src/main/java/com/ssafy/apm/common/service/SocketService.java
@@ -1,6 +1,10 @@
 package com.ssafy.apm.common.service;
 
+import com.ssafy.apm.common.dto.SessionResponseDto;
+
 public interface SocketService {
+
+    SessionResponseDto getSession(String sessionId);
 
     void addSession(String sessionId);
 
