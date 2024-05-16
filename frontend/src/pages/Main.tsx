802adcb98e9531593f5accab6506c11419bcba52
yeogeru <yjw0835@naver.com>
2024-05-16 15:08:31 +0900

    fix: game ui fix

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -7,11 +7,15 @@ const MainPage = () => {
   const navigate = useNavigate();
   const [curpage, setCurpage] = useState<boolean>(false);
   const [mainLoad, setMainLoad] = useState<boolean>(false);
+  const [logoMove, setLogoMove] = useState<boolean>(false);
   useEffect(() => {
     setTimeout(() => {
       setMainLoad(true);
     }, 1000);
   }, []);
+  const logoMoveFunc = () => {
+    setLogoMove(true);
+  }
   /**
    * 
    * 회원가입 페이지 ui 구현, 채널 선택으로 라우팅, 설정 및 도움말 modal표시
@@ -20,7 +24,7 @@ const MainPage = () => {
    */
   return (
     <div className="flex flex-col items-center w-1/2 z-10">
-      <div className="min-w-[100vw] flex flex-col items-center justify-center mb-4 animate-logo">
+      <div className={`min-w-[100vw] flex flex-col items-center justify-center mb-4 animate-logo transition duration-1000 ${logoMove?"-translate-y-[100vh]":""}`}>
         <div className="w-full h-28 bg-[url(/public/ui/logo_main.png)] bg-no-repeat bg-contain bg-center" />
         <div className="w-full h-20 bg-[url(/public/ui/logo_sub.png)] bg-no-repeat bg-contain bg-center" />
       </div>
@@ -38,6 +42,7 @@ const MainPage = () => {
                 movePage={() => {
                   setCurpage(true);
                 }}
+                logoMoveFunc={logoMoveFunc}
               />
             )}
           </div>
